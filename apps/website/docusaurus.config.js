0fea5cead65a47907208c06dfa9fb27ff88d3d6a
Paul Schultz <pschultz@pobox.com>
2023-05-18 13:56:59 -0500

    Remove google analytics
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -95,10 +95,6 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        gtag: {
-          trackingID: process.env.G_TAG || 'G_TAG',
-          anonymizeIP: true,
-        },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
